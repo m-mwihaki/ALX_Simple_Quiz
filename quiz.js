a501e3fc3087033fed6f184aa77e5f70ae899a42
m-mwihaki <m.mwihakiann@gmail.com>
2025-10-28 22:00:07 +0300

    second commit

--- quiz.js
+++ quiz.js
@@ -1,8 +1,12 @@
+
 function checkAnswer() {
+    
     const correctAnswer = "4";
 
-    constselectedOption = document.querySelector('input[name="quiz"]:checked');
+    
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
 
+    
     if (!selectedOption) {
         document.getElementById("feedback").textContent = "Please select an answer before submitting.";
         return; 
@@ -10,17 +14,16 @@ function checkAnswer() {
 
     const userAnswer = selectedOption.value;
 
+    
     if (userAnswer === correctAnswer) {
-        document.getElementById("feedback").textContent = "Correct! Well done";
+        document.getElementById("feedback").textContent = "Correct! Well done.";
     } else {
         document.getElementById("feedback").textContent = "That's incorrect. Try again!";
     }
-
 }
+
 
 document.addEventListener("DOMContentLoaded", function() {
     const submitButton = document.getElementById("submit-answer");
-    if (submitButton) {
-        submitButton.addEventListener("click", checkAnswer);
-    }
+    submitButton.addEventListener("click", checkAnswer);
 });
